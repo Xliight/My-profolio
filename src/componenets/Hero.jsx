e80538f4f4d9c90983c0a75b0a177919d1f217d1
Abdelfettah MOSTAKIR <112322308+Xliight@users.noreply.github.com>
2024-12-19 03:46:44 -0800

    Update Hero.jsx

--- src/componenets/Hero.jsx
+++ src/componenets/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
                       variants={container(1)}
                       initial='hidden' animate='visible'
                       className="inline-block rounded-xl border border-indigo-600 bg-indigo-600 px-6 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-green-500 focus:outline-none focus:ring active:text-indigo-500"
-                          href="/CV Abdelfettah_Mostakir.pdf" target="_blank"
+                          href="/Abdelfettah_Mostakir.pdf" target="_blank"
                         >
                           Download CV "Fr"</motion.a>
                       <motion.a 
